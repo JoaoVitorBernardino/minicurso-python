eb629f14b86a989f653ca08fb3bcafb7af3e2a78
JoaoVitorBernardino <jvitorbernardino@gmail.com>
2024-10-17 02:25:53 -0300

    adds examples of assignment operators

---
+++ 1-basic/4-assignment-operators.py
@@ -0,0 +1,27 @@
+a = 0
+a += 10
+print(a)
+
+b = 10
+b -= 5
+print(b)
+
+c = 5
+c *= 2
+print(c)
+
+d = 10
+d /= 2
+print(d)
+
+e = 10
+e %= 2
+print(e)
+
+f = 5
+f **= 2
+print(f)
+
+g = 10
+g //= 2
+print(g)
